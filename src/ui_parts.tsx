ce5d51ee7c7c766a52cec5496a53557ae008c876
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2025-08-20 14:54:22 +0900

    refactor: remove unused theme related logic

--- src/ui_parts.tsx
+++ src/ui_parts.tsx
@@ -39,39 +39,26 @@ const ToolBar = (props: {store: Store;}) => {
         case "menu_load": openFile(); break;
         case "menu_keyboard_shortcuts": store.trigger(ACTION.DIALOG_HELP_OPEN); break;
         case "menu_settings": store.trigger(ACTION.SHOW_SETTINGS, !store.showSettings); break;
-        // case "set-dark": store.trigger(ACTION.CHANGE_UI_THEME, "dark"); break;
-        // case "set-light": store.trigger(ACTION.CHANGE_UI_THEME, "light"); break;
         }
         setSelectedKey(0);
     };
     const [selectedKey, setSelectedKey] = useState(0);
 
-    const [theme, setTheme] = useState(store.uiTheme); // 現在のテーマを管理
     useEffect(() => { // マウント時
-        store.on(CHANGE.CHANGE_UI_THEME, () => {
-            setTheme(store.uiTheme);
-        });
     }, []);
 
     return (
         <Navbar expand={true} 
-            variant="dark" // ここは dark のままの方がいいかも
-            style={{ backgroundColor: theme == "dark" ? "#272a31": "#3E455E"}}
+            variant="dark"
+            style={{ backgroundColor: "#272a31"}}
         >
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
             <Nav onSelect={dispatch} activeKey={selectedKey}>
-                <NavDropdown menuVariant={theme} title={<BsList size={20}/>} id="collapsible-nav-dropdown">
+                <NavDropdown menuVariant="dark" title={<BsList size={20}/>} id="collapsible-nav-dropdown">
                     <NavDropdown.Item eventKey="menu_load">
                         Load file
                     </NavDropdown.Item>
-                    {/* <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey="set-dark" active={theme === "dark"}>
-                        {theme === "dark" && <i className="bi bi-check"></i>} Dark
-                    </NavDropdown.Item>
-                    <NavDropdown.Item eventKey="set-light" active={theme === "light"}>
-                        {theme === "light" && <i className="bi bi-check"></i>} Light
-                    </NavDropdown.Item> */}
                     <NavDropdown.Item eventKey="menu_settings">
                         Settings
                     </NavDropdown.Item>
@@ -85,7 +72,7 @@ const ToolBar = (props: {store: Store;}) => {
                 </NavDropdown>
             </Nav>
             <Nav onSelect={dispatch} activeKey={selectedKey}
-                style={{ color: theme == "dark" ? "#C9CACB" : "#ffffff" }} className="me-auto" // このクラスでリンクが左側に配置される
+                style={{ color: "#C9CACB" }} className="me-auto" // このクラスでリンクが左側に配置される
             >
                 {/* <Nav.Link className="nav-link tool-bar-link" eventKey="zoom-in">
                     <i className="bi bi-zoom-in"></i> Zoom In                
@@ -102,12 +89,8 @@ const ToolBar = (props: {store: Store;}) => {
 const StatusBar = (props: {store: Store;}) => {
     let store = props.store;
     const [statusBarMessage, setStatusBarMessage] = useState("");
-    const [theme, setTheme] = useState(store.uiTheme); // 現在のテーマを管理
 
     useEffect(() => { // マウント時
-        store.on(CHANGE.CHANGE_UI_THEME, () => {
-            setTheme(store.uiTheme);
-        });
         store.on(CHANGE.MOUSE_MOVE, (message: string) => {
             setStatusBarMessage(message);
         });
@@ -117,11 +100,11 @@ const StatusBar = (props: {store: Store;}) => {
         // {/* <div style={{ height: "40px", backgroundColor: "#eee", padding: "10px", textAlign: "left", borderTop: "1px solid #ccc" }}>
         //     <span>{statusBarMessage}</span> */}
         <div style={{ height: "30px", minHeight: "30px", 
-            backgroundColor: theme == "dark" ? "#272a31": "#FAFAFA", 
+            backgroundColor: "#272a31", 
             paddingLeft: "10px", 
-            textAlign: "left", borderTop: "0.5px solid " + theme == "dark" ? "#383B41" : "#C6C6C6" }}
+            textAlign: "left", borderTop: "0.5px solid #383B41"}}
         >
-            <span style={{ color: theme == "dark" ? "#C9CACB" : "#191919", fontSize: "15px" }}>{statusBarMessage}</span>
+            <span style={{ color: "#C9CACB", fontSize: "15px" }}>{statusBarMessage}</span>
         </div>
     );
 };
